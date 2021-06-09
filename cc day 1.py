65e86e35eca4fcd34f6ee27f38e13d9c4e4b31cd
assa <assasingh.1991.com>
2021-06-09 08:07:46 -0700

    cc/assignment day 1

---
+++ cc day 1.py
@@ -0,0 +1,20 @@
+
+print("please enter your name")
+name=input()
+print("name of data type is", type(name))
+
+print("your age")
+x=int(input())
+print("age of data type is", type(x))
+
+print("your mobile no ")
+y=int(input())
+print("mobile no data is", type(y))
+
+print("enter your adress")
+z=(input())
+print("adress data is", type(z))
+
+
+
+
